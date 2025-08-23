e6b95079d08a5329e0b2184f211a1aca05eb416d
MrDelfin120 <lujan.edgar@uabc.edu.mx>
2025-08-22 23:28:10 -0700

    Update Automate_UCC_Done.py

--- HDM Work/Automate_UCC_Done.py
+++ HDM Work/Automate_UCC_Done.py
@@ -4,7 +4,8 @@ Created on Wed Aug 20 16:01:49 2025
 
 @author: Dell
 """
-
+#This script takes a .txt and an excel file from UCC filing to automate the process of updating the excel. Deals that appear in the .txt are supposed to change their status from sent
+#to done.
 import pandas as pd
 from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import askdirectory
@@ -27,7 +28,7 @@ Directory= askdirectory(title="Select directory")
 # just use read_excel from pd and it will make the work easier
 
 UCC = pd.read_excel(path)
-UCC = UCC.sort_values(by='DealName', ascending=True)  # If ascending=False, it's the same as typing 'descend' in MATLAB.
+UCC = UCC.sort_values(by='Deal Name', ascending=True)  # If ascending=False, it's the same as typing 'descend' in MATLAB.
 UCC_Deal = UCC.copy()  # Create a copy to ensure no overwriting.
  
 deal_list = []  # create the deal name List to work with.
@@ -43,12 +44,12 @@ i = 0
 not_found = []
 
 for deal in deal_list:
-    # if any(UCC_Deal.loc[UCC_Deal['DealName'] == deal]):
+    # if any(UCC_Deal.loc[UCC_Deal['Deal Name'] == deal]):
     # It "works" but not for the reason you might expect—it’s not truly checking “does at least one row match my condition,”
     # but rather “is there any truthy value in these columns,”
     # which just happens to align with your goal in this case.
-    if not UCC_Deal.loc[UCC_Deal['DealName'] == deal].empty:  # This approach is more adequate since it actually follows my logic.
-        UCC_Deal.loc[UCC_Deal['DealName'] == deal, 'UCCFiled'] = 'Done'
+    if not UCC_Deal.loc[UCC_Deal['Deal Name'] == deal].empty:  # This approach is more adequate since it actually follows my logic.
+        UCC_Deal.loc[UCC_Deal['Deal Name'] == deal, 'UCC Filed'] = 'Done'
         i += 1
     else:
         not_found.append(deal)
@@ -66,19 +67,19 @@ output_path = os.path.join(Directory, "not_found.txt") #el resultado es la direc
 not_found
 not_found_deal = []
 try: 
-    with open(output_path, "x") as missing_list:
+    with open(output_path, "x") as missing_list: #this will attempt to create a new file, if non exists, no issue will show.
         for name in not_found: 
             not_found_deal.append(f"{name}\n")
         missing_list.writelines(not_found_deal)
         print("\nMissing deals have been written to 'not_found.txt'")
 
-except FileExistsError:
+except FileExistsError: #If the file is in your directory then this will execute. Please make sure to check the content of your file.
     print(f"\nThe file already exists. Make sure it has the right information. check in: {Directory}")
     choice = int(input("\nIs the content of the file correct? Type 1 for yes and 2 for no: "))
-    if choice == 1:
+    if choice == 1: #by typing 1 you are saying that the existent file contains the correct information.
         sys.exit()
     else:
-        with open(output_path, "w") as missing_list:
+        with open(output_path, "w") as missing_list: #This will rewrite to the file.
             for name in not_found: 
                 not_found_deal.append(f"{name}\n")
             missing_list.writelines(not_found_deal)
